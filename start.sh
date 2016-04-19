732db4ae058ee0181fac262878895f52d9937938
HackHusky <kurst811@gmail.com>
2016-04-18 18:49:35 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
  node app.js &
  
  # start camera 
- $ uv4l --driver raspicam --auto-video_nr --width 640 --height 480 --encoding h264 --framerate 20 --vflip yes --hflip yes
+ uv4l --driver raspicam --auto-video_nr --width 640 --height 480 --encoding h264 --framerate 20 --vflip yes --hflip yes
